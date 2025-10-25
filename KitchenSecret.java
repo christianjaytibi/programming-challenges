0ba1fc51b24a2dfc683d114b524baa7be8edff9f
Christian Jay Tibi <145688353+christianjaytibi@users.noreply.github.com>
2025-10-25 14:02:47 +0800

    Solved Hyperskill 53138 - The Kitchen Secret

---
+++ KitchenSecret.java
@@ -0,0 +1,62 @@
+/*
+*   Problem: The Kitchen Secret
+*   Problem Link: https://hyperskill.org/learn/daily/53138
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class KitchenSecret {
+    static final String filePath = "hyperskill-dataset-117344221.txt";
+
+    public static void main(String[] args) throws IOException {
+        Path path  = Path.of(filePath);
+
+        BufferedReader reader = Files.newBufferedReader(path);
+        reader
+            .lines()
+            .map(str -> {
+                String[] instructions = str.split(",");
+                return processInstruction(instructions);
+            })
+            .forEach(System.out::print);
+
+        reader.close();
+    }
+
+    private static char processInstruction(String[] instructions) {
+        char[][] letters = new char[][] {
+            {'A', 'B', 'C', 'D'},
+            {'E', 'F', 'G', 'H'},
+            {'I', 'J', 'K', 'L'},
+            {'M', 'N', 'O', 'P'}
+        };
+
+        int row = 0;
+        int col = 0;
+        int lowerBound = 0;
+        int upperBound = letters.length - 1;
+
+        for (String instruction : instructions) {
+            if (instruction.equals("UP") && row == lowerBound)
+                continue;
+            else if (instruction.equals("DOWN") && row == upperBound)
+                continue;
+            else if (instruction.equals("LEFT") && col == lowerBound)
+                continue;
+            else if (instruction.equals("RIGHT") && col == upperBound)
+                continue;
+
+            switch (instruction) {
+                case "UP" -> row--;
+                case "DOWN" -> row++;
+                case "LEFT" -> col--;
+                case "RIGHT" -> col++;
+            }
+        }
+
+        return letters[row][col];
+    }
+}
