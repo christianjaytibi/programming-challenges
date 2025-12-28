cb41bea732b81de0898639f691b151b11dfbd42a
Christian Jay Tibi <145688353+christianjaytibi@users.noreply.github.com>
2025-12-28 13:47:04 +0800

    Refactor Hyperskill 53345 - On The Fourth Day Of Coding...

--- OnTheFourthDayOfCoding.java
+++ OnTheFourthDayOfCoding.java
@@ -3,22 +3,19 @@
 *   Problem Link: https://hyperskill.org/learn/daily/53345
 * */
 
+package twelvedaysofcoding;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class OnTheFourthDayOfCoding {
-    private static final String fileName = "hyperskill-dataset-119026486.txt";
+    private static final String fileName = "out/production/LearnJava/twelvedaysofcoding/hyperskill-dataset-119026486.txt";
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(fileName));
 
-        Map<String, Boolean> state = new HashMap<>();
-        for (int i = 0; i < 4; i++) {
-            state.put(String.format("Fork_%d", i + 1), false);
-        }
+        boolean[] state = new boolean[4];
 
         int contentions = 0;
         while (sc.hasNextLine()) {
@@ -28,14 +25,13 @@ public class OnTheFourthDayOfCoding {
 
             String[] log = line.split(",");
             String action = log[1];
-            int fork = Integer.parseInt(log[2]);
-            String forkId = String.format("Fork_%d", fork);
+            int fork = Integer.parseInt(log[2]) - 1;
 
             if (action.equals("pick")
-                && state.get(forkId))
+                && state[fork])
                 contentions++;
             else
-                state.put(forkId, !state.get(forkId));
+                state[fork] = !state[fork];
         }
         sc.close();
 
